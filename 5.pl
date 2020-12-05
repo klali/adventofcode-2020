8b7494c10df882c34f967d3a64181302ec404e52
Klas Lindfors <klali@avm.se>
2020-12-05 10:43:05 +0100

    5: avoid using an array at all

--- 5.pl
+++ 5.pl
@@ -4,10 +4,10 @@ my $m = shift || 1;
 
 while(<>) {
   tr/FBLR/0101/;
-  $p{oct("0b" . $_)} = 1;
+  $d = oct("0b" . $_);
+  $j = $d if $d > $j;
+  $p{$d} = 1;
 }
-@p = sort {$a <=> $b} keys(%p);
-($j) = grep(!$p{$_}, (8..$#p)) if $m eq 2;
-$j = $p[$#p] if $m eq 1;
+($j) = grep(!$p{$_}, (8..$j)) if $m eq 2;
 
 warn $j;
